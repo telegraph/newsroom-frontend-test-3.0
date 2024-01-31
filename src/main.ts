6325dddcc1e01fc4de455b499b95ada7ba848c4d
Gliga Daniel Dumitru <gliga.dumitru@3pillarglobal.com>
2024-01-31 16:50:18 +0200

    update base logic

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>
-  <h1>Country Information Search</h1>
-</div>
-`
-
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Welcome to newsroom-frontend-test-2.0')
+  document.querySelector<HTMLHeadingElement>('#title')!.innerText = 'Country Information Search';
 })
